35adc333a44dfb72a12f3a20ddb226b57d418dc1
AlTesla <alvaro.corona91@gmail.com>
2024-03-31 02:40:01 -0700

    pandas data frame created

--- dataPortfolio/pages/pokedata.py
+++ dataPortfolio/pages/pokedata.py
@@ -1,10 +1,15 @@
 import reflex as rx
 from dataPortfolio.components.sidebar import sidebar
+import pandas as pd
 
 
 
-path = "https://github.com/ACRivera95/project-3.1/blob/main/Output/Pokemon_data.json"
+path = "https://mydatabucket-altesla.s3.us-west-1.amazonaws.com/Pokemon_data.csv"
 
+
+def dataframe():
+    poke_frame = pd.read_csv(path)
+    return poke_frame
 
 
 @rx.page(route="/pokedata", title="Pokedata")
@@ -12,6 +17,16 @@ def pokedata() -> rx.Component:
     return rx.hstack(
         sidebar(),
         rx.vstack(
-            rx.heading("Pokedata Visulization", as_="h1", size="9", align="center", width="1600px")
-        )
+            rx.heading("Pokedata Visulization", as_="h1", size="9", align="center", width="1600px"),
+            rx.spacer(), 
+            rx.box(
+                rx.data_table(
+                    data = dataframe()[["Name"]],
+                    search = True,
+                    )
+                ),
+                width="15em",
+                height="5em"
+
+            )
     )
