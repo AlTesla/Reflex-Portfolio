46101911237b0896f58f0f3bb8e5550ce31a6462
AlTesla <alvaro.corona91@gmail.com>
2024-04-05 00:26:17 -0700

    strongest name and images created and working

--- dataPortfolio/pages/pokedata.py
+++ dataPortfolio/pages/pokedata.py
@@ -11,31 +11,31 @@ gens = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
 
 class PokeState(rx.State):
     gen = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
-    selected_gen: str ='1st'
+    selected_gen: str ='4th'
 
     @rx.var
     def gen_df(self) -> pd.DataFrame:
         pk_df = poke_frame[poke_frame["Generation"] == self.selected_gen].copy()
         return pk_df
 
-
+    """
     @rx.var
     def max_row(self) -> pd.DataFrame:
-        #strongest_row = self.gen_df.loc[self.gen_df['Total'].idxmax()]
-        strongest_row = self.gen_df[self.gen_df['Total'] == self.gen_df['Total'].max()]
+        strongest_row = self.gen_df.loc[self.gen_df['Total'].idxmax()].to_frame().T
+        #strongest_row = self.gen_df[self.gen_df['Total'] == self.gen_df['Total'].max()]
         return strongest_row
-    
+    """    
     @rx.var
     def max_name(self) -> str:
-        strongest_name = self.max_row['Name'].squeeze()
+        strongest_row = self.gen_df.loc[self.gen_df['Total'].idxmax()].to_frame().T
+        strongest_name = strongest_row['Name'].squeeze()
         return strongest_name
     
     @rx.var
     def max_image(self) -> str:
-        strongest_image = self.max_row["ImageUrl"].squeeze()
+        strongest_row = self.gen_df.loc[self.gen_df['Total'].idxmax()].to_frame().T
+        strongest_image = strongest_row["ImageUrl"].squeeze()
         return strongest_image
-    
-
 
 
 def gen_selector() -> rx.Component:
@@ -57,7 +57,7 @@ def gen_selector() -> rx.Component:
 def strongest() -> rx.Component:
     return rx.box(
         rx.text(PokeState.max_name),
-        rx.image(src=PokeState.max_image, width='12em', height="auto")
+        rx.image(src=PokeState.max_image, width = "12em", height="auto")
         
     )
 
