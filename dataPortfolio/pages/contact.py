6889591f3dc5c7b1f495d5b1fa4c19233ea85eb6
AlTesla <alvaro.corona91@gmail.com>
2024-05-26 12:51:48 -0700

    contact updated

--- dataPortfolio/pages/contact.py
+++ dataPortfolio/pages/contact.py
@@ -2,10 +2,24 @@ import reflex as rx
 
 class EditorState(rx.State):
     content:str = "<p>Editor content</p>"
+    
+    def handle_change(self, content: str):
+        self.content = content
 
 
 
 
 @rx.page(route="/contact", title="Contact")
 def contact() -> rx.Component:
-    return
+    return rx.vstack(
+        rx.editor(
+            set_contents=EditorState.content,
+            on_change=EditorState.handle_change,
+        ),
+        rx.box(
+            rx.html(EditorState.content),
+            border="1px dashed #ccc",
+            border_radius="0.25em"
+            width="100%",
+        ),
+    )
