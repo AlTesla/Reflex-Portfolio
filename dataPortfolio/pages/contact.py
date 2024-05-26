2e889b60613dd594862c12a765ce2346d8a8561b
AlTesla <alvaro.corona91@gmail.com>
2024-05-25 23:23:50 -0700

    contact updated

--- dataPortfolio/pages/contact.py
+++ dataPortfolio/pages/contact.py
@@ -1,1 +1,11 @@
 import reflex as rx
+
+class EditorState(rx.State):
+    content:str = "<p>Editor content</p>"
+
+
+
+
+@rx.page(route="/contact", title="Contact")
+def contact() -> rx.Component:
+    return
