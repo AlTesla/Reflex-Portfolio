4fd3efce095129575ced6100b392327f981b30df
AlTesla <alvaro.corona91@gmail.com>
2024-06-05 23:41:38 -0700

    steam updated

--- dataPortfolio/dataPortfolio.py
+++ dataPortfolio/dataPortfolio.py
@@ -3,5 +3,5 @@ from dataPortfolio.pages.index import index
 from dataPortfolio.pages.pokedata import pokedata
 from dataPortfolio.pages.contact import contact
 from dataPortfolio.pages.dashboard import dashboard
-
+from dataPortfolio.pages.steam import steam 
 app = rx.App()
--- dataPortfolio/pages/steam.py
+++ dataPortfolio/pages/steam.py
@@ -5,3 +5,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+@rx.page(route="/steam", title="Steam")
+def steam() -> rx.Component:
+    return rx.hstack(
+        sidebar(),
+    )
